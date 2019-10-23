45a4fcdfc88c241f30551555654ee322c8a06ca9
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-23 14:50:39 -0500

    finished seeds in development env

--- data/seeds/05-punchpass.js
+++ data/seeds/05-punchpass.js
@@ -1,13 +1,18 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {client_id: 1, instructor_id: 1, passes: 1},
+        {client_id: 2, instructor_id: 1, passes: 1},
+        {client_id: 3, instructor_id: 1, passes: 1},
+        {client_id: 4, instructor_id: 2, passes: 1},
+        {client_id: 5, instructor_id: 2, passes: 1},
+        {client_id: 6, instructor_id: 2, passes: 1},
+        {client_id: 7, instructor_id: 3, passes: 1},
+        {client_id: 8, instructor_id: 3, passes: 1},
+        {client_id: 9, instructor_id: 3, passes: 1},
+        {client_id: 10, instructor_id: 4, passes: 1},
+        {client_id: 11, instructor_id: 4, passes: 1},
+        {client_id: 12, instructor_id: 4, passes: 1},
       ]);
-    });
 };
