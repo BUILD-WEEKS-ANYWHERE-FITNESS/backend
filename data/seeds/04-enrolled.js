d988dafac0529894e36e9c60b2317e831a8cfb93
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-23 14:44:34 -0500

    added seed to all but punchpass table

--- data/seeds/04-enrolled.js
+++ data/seeds/04-enrolled.js
@@ -1,13 +1,25 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+ 
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {client_id: 1, class_id: 1},
+        {client_id: 2, class_id: 1},
+        {client_id: 3, class_id: 1},
+        {client_id: 4, class_id: 2},
+        {client_id: 5, class_id: 2},
+        {client_id: 6, class_id: 2},
+        {client_id: 7, class_id: 3},
+        {client_id: 8, class_id: 3},
+        {client_id: 9, class_id: 3},
+        {client_id: 10, class_id: 4},
+        {client_id: 11, class_id: 4},
+        {client_id: 12, class_id: 4},
+        {client_id: 13, class_id: 5},
+        {client_id: 1, class_id: 5},
+        {client_id: 2, class_id: 5},
+        {client_id: 3, class_id: 6},
+        {client_id: 4, class_id: 6},
+        {client_id: 5, class_id: 6}
       ]);
-    });
 };
