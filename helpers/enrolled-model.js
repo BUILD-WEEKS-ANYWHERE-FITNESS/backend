2e2f83a3dd54cbc1a501b76bb079048bd4df8326
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-29 08:33:10 -0500

    enrolled model using COALESCE

--- helpers/enrolled-model.js
+++ helpers/enrolled-model.js
@@ -9,10 +9,11 @@ function clientsEnrolled(){
     return db('enrolled')
         .select('classes.id', 'instructors.username as instructor', 'name', 'type', 'start_time', 'duration', 'intensity', 'location', 'max_size')
         .join('classes', 'enrolled.class_id','classes.id')
-        .leftJoin('clients', 'enrolled.client_id','clients.id')
-        .leftJoin('instructors', 'classes.instructor_id', 'instructors.id')
-        .count('enrolled.client_id as atendees')
+        .join('clients', 'enrolled.client_id','clients.id')
+        .join('instructors', 'classes.instructor_id', 'instructors.id')
+        // .count('enrolled.client_id as atendees')
         .groupBy('enrolled.class_id')
+        .raw('COALESCE(COUNT(enrolled.client_id as atendees), 0)')
 }
 
 function classwithClients() {
