777cbed78b04803f600d1cf38ae14f8f23c77808
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-29 09:16:51 -0500

    added enrolled in the select clause

--- helpers/enrolled-model.js
+++ helpers/enrolled-model.js
@@ -18,6 +18,18 @@ function clientsEnrolled(){
 
 function classwithClients() {
     return db('classes')
+    .select('enrolled.class_id', 'enrolled.instructors.username', 'enrolled.classes.name', 'enrolled.classes.type', 'enrolled.classes.start_time', 'enrolled.classes.duration', 'enrolled.classes.intensity', 'enrolled.classes.location', 'enrolled.classes.max_size', db.raw('COALESCE(COUNT(enrolled.client_id), 0)'))
+    .join('enrolled', 'enrolled.class_id', 'classes.id')
+    .join('instructors', 'classes.instructor_id', 'instructors.id')
+    .join('clients', 'clients.id', 'enrolled.client_id')
+    // .count('enrolled.client_id as clients_attending')
+    // .raw('COALESCE(COUNT(enrolled.client_id as atendees), 0)')
+    .groupBy('enrolled.class_id')
+}
+
+/*
+function classwithClients() {
+    return db('classes')
     .select('classes.id', 'instructors.username', 'classes.name', 'classes.type', 'classes.start_time', 'classes.duration', 'classes.intensity', 'classes.location', 'classes.max_size', db.raw('COALESCE(COUNT(enrolled.client_id), 0)'))
     .join('enrolled', 'enrolled.class_id', 'classes.id')
     .join('instructors', 'classes.instructor_id', 'instructors.id')
@@ -26,4 +38,5 @@ function classwithClients() {
     // .raw('COALESCE(COUNT(enrolled.client_id as atendees), 0)')
     .groupBy('classes.id')
 }
+*/
 
